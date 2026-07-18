b50aecaa48ff1ae4d9261baaca916f210488f751
Anand kumar Prajapati <121502349+anandkumarprajap@users.noreply.github.com>
2026-07-18 22:40:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# This code is from https://github.com/LondheShubham153/flask-app-ecs/tree/main
+# This code is from https://github.com/anandkumarprajap/GitHub-Actions/edit/main/app.py
 # Flask App
 from flask import Flask, render_template
 app = Flask(__name__)
